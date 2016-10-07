fd232538cd1fe21fcf076689990ac53adc14b220
Marcin_Debian <cinekqw1@gmail.com>
2016-10-07 17:53:22 +0200

    devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,56 @@
 class ItemsController < ApplicationController
 
-def index
-	
-end
+before_action :find_item, only: [:show, :edit,:update,:destroy]
+
+	def index
+		@items = Item.all.order("created_at DESC")
+	end
 
 
-def new
-	@item = Item.new
-end
+	def new
+		@item = Item.new
+	end
 
-def create
-	
-end
+	def create
+		@item = Item.new(item_params)
+		if @item.save
+		redirect_to root_path
+		else
+			render 'new'
+		end 
 
+	end
+
+	def edit
+
+	end
+
+
+	def update
+		if @item.update(item_params)
+			redirect_to items_path(@item)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to root_path
+	end
+
+	def show
+		
+	end
+
+private
+
+	def item_params
+		params.require(:item).permit(:title,:description)
+		
+	end
+
+	def find_item
+		@item = Item.find(params[:id])
+	end
 end
