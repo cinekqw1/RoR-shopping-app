e1e53f2fb4bc23ba7ef0f4e035d2deb042e060bd
Marcin_Debian <cinekqw1@gmail.com>
2016-11-10 22:23:12 +0100

    api_controller added:complete(), creatte_item(), destroy()

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
 
 skip_before_filter  :verify_authenticity_token
-before_action :restrict_access, only: [:items]
+before_action :restrict_access, only: [:items, :create_item, :destroy,:complete]
 
 
 
@@ -33,16 +33,53 @@ $current_token
 	end
 
 
-	#curl -X POST -H "Authorization: Token token=e6628321336ecbbb91b1819848a10d14" http://localhost:3000/api/items
+	#curl -X POST -H "Authorization: Token token=ed0a992aef6d7a1bedd54163d295ded8" http://localhost:3000/api/items
 	def items
 		@user = User.find_by_id($current_token.user_id)
 		@items = Item.where(:user_id => @user.id).order("created_at DESC")
 		render :json => 
-  			@items.to_json(:only => [:title, :description])
-
+  			@items.to_json(:only => [:id, :title, :description, :completed_at])
+  			#@items.count
 	end
 
-	#curl -X POST -H "Authorization: Token token=c96044f1bce9a526513a5073d9ea9bcb" http://localhost:3000/api/logout
+	#curl -H "Content-Type: application/json" -H "Authorization: Token token=ed0a992aef6d7a1bedd54163d295ded8" -X POST -d '{"item": {"title":"Item_1","description":"This is destription of this item. In here may be everything what you want"}}' http://localhost:3000/api/createitem
+	def create_item
+		@user = User.find_by_id($current_token.user_id)
+		
+		
+		@item = @user.items.build(params.require(:item).permit(:title,:description))
+		if @item.save
+			render json: {:status => "item created"}
+		else
+			render json: {:status => "item created error"}
+		end 
+	end
+	
+	
+	#curl -H "Content-Type: application/json" -H "Authorization: Token token=ed0a992aef6d7a1bedd54163d295ded8" -X POST -d '{"item": {"id":"34"}}' http://localhost:3000/api/complete
+
+	def complete
+		@item = Item.find(params[:item][:id])
+		if @item.update_attribute(:completed_at, Time.now)
+			render json: {:status => "marked succesfull"}
+		else
+			render json: {:status => "marked error"}
+		end
+	end
+
+	#curl -H "Content-Type: application/json" -H "Authorization: Token token=ed0a992aef6d7a1bedd54163d295ded8" -X POST -d '{"item": {"id":"34"}}' http://localhost:3000/api/destroy
+	def destroy
+	
+		
+		@item = Item.find_by_id(params[:item][:id])
+		if @item.nil?
+			render json: {:status => "item deleted error"}
+			 
+		else 
+			 @item.destroy
+			 render json: {:status => "item deleted"}
+		end
+	end
 
 	def logout
 
@@ -64,13 +101,13 @@ private
 	        
 	        if ($current_token.expires_at - Time.now) < 0
 	          $current_token.destroy
-	          render json: {:message => "token expired error"}
+	          render json: {:status => "token expired error"}
 	        else
 	          true
 	        end
 
 	      else
-	        render json: {:message => "invalid token"}
+	        render json: {:status => "invalid token"}
 	      end
 	    end
 	    
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   match '/api' => 'api#login', via: :post ,format: :json
   match '/api/items' => 'api#items', via: :post ,format: :json
   match '/api/logout' => 'api#logout', via: :post ,format: :json
+  match '/api/createitem' => 'api#create_item', via: :post ,format: :json
+  match '/api/destroy' => 'api#destroy', via: :post ,format: :json
+  match '/api/complete' => 'api#complete', via: :post ,format: :json
 end
