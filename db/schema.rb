a45c3f22a972cc05ad17c5d8bc331e464f435475
Marcin_Debian <cinekqw1@gmail.com>
2016-12-26 12:13:14 +0100

    backup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015200440) do
+ActiveRecord::Schema.define(version: 20161223200900) do
 
   create_table "api_keys", force: :cascade do |t|
     t.string   "access_token"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20161015200440) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.datetime "completed_at"
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
